4a8c77c8e7031b8cb298206d8c05b228bfa43cf8
Keanu Williams <keanucwilliams@icloud.com>
2021-11-12 15:55:37 -1000

    Cleaned up code to begin creating chat tool

--- client.py
+++ client.py
@@ -1,7 +1,32 @@
 ##########################################################################
 #        Name: client.py
-# Description: 
+# Description: Connects to the server to attempt to join a chat room to
+#              chat with others that join the same chat room.
 #  Created on: November 10, 2021
-#  Updated on: November 10, 2021
+#  Updated on: November 12, 2021
 #      Author: Keanu Williams
 ##########################################################################
+# Imports
+import socket
+
+##########################################################################
+# Macros
+HEADER_LENGTH = 128
+HEADER_FORMAT = "utf-8"
+IP_ADDRESS = socket.gethostbyname(socket.gethostname())
+SERVER_IP_ADDRESS = "192.168.1.144"
+PORT = 5050
+DISCONNECT_MSG = "!DISCONNECT"
+
+##########################################################################
+
+class Client:
+  def __init__(self) -> None:
+    self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.client.connect((SERVER_IP_ADDRESS, PORT))
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
--- server.py
+++ server.py
@@ -1,30 +1,42 @@
 ##########################################################################
 #        Name: server.py
-# Description: 
+# Description: The server handling all of the clients connecting to chat.
 #  Created on: November 10, 2021
-#  Updated on: November 10, 2021
+#  Updated on: November 12, 2021
 #      Author: Keanu Williams
 ##########################################################################
-
+# Imports
 import socket
 import threading
 
-PORT = 5050
+##########################################################################
+# Macros
+HEADER_LENGTH = 128
+HEADER_FORMAT = "utf-8"
 IP_ADDRESS = socket.gethostbyname(socket.gethostname())
+PORT = 5050
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+##########################################################################
 
 class Server:
   def __init__(self) -> None:
-      self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      self.server.bind((IP_ADDRESS, PORT))
-  
-  def handle_client(self):
-    pass
+    self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.server.bind((IP_ADDRESS, PORT))
+
+  def handle_connection(self, connection, address):
+    connection.close()
 
   def start(self) -> None:
-    pass
+    self.server.listen()
+    while True:
+      connection, address = self.server.accept()
+      thread = threading.Thread(target=self.handle_connection, args=(connection, address))
+      thread.start()
 
 def main():
-  pass
+  server = Server()
+  server.start()
 
 if __name__ == "__main__":
   main()
