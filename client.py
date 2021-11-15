6e7e79ecf42b45e8fd0bf37419450eb414bed896
Keanu Williams <keanucwilliams@icloud.com>
2021-11-14 22:52:53 -1000

    Updated client to be able to send and receive plain text to server

--- client.py
+++ client.py
@@ -3,7 +3,7 @@
 # Description: Connects to the server to attempt to join a chat room to
 #              chat with others that join the same chat room.
 #  Created on: November 10, 2021
-#  Updated on: November 12, 2021
+#  Updated on: November 14, 2021
 #      Author: Keanu Williams
 ##########################################################################
 # Imports
@@ -14,7 +14,7 @@ import socket
 HEADER_LENGTH = 128
 HEADER_FORMAT = "utf-8"
 IP_ADDRESS = socket.gethostbyname(socket.gethostname())
-SERVER_IP_ADDRESS = "192.168.1.144"
+SERVER_IP_ADDRESS = "192.168.1.29"
 PORT = 5050
 DISCONNECT_MSG = "!DISCONNECT"
 
@@ -23,10 +23,46 @@ DISCONNECT_MSG = "!DISCONNECT"
 class Client:
   def __init__(self) -> None:
     self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  def start(self) -> None:
+    print(f"Connecting to server at {SERVER_IP_ADDRESS}:{PORT}...", end="")
     self.client.connect((SERVER_IP_ADDRESS, PORT))
+    print("Connection successful")
+    connected = True
+    while connected:
+      self.receive()
+      msg_to_send = ""
+      while msg_to_send == "":
+        msg_to_send = input(">> ")
+      if DISCONNECT_MSG in msg_to_send.upper():
+        connected = False
+        print("Disconnecting from server...", end="")
+      self.send(msg_to_send)
+    print("Disconnected successfully")
+      
+  def receive(self) -> None:
+    print(self.client.recv(2048).decode(HEADER_FORMAT))
+
+  def send(self, msg) -> None:
+    # Encode the message in the correct format
+    msg_to_send = msg.encode(HEADER_FORMAT)
+    # Get the length of the message
+    msg_length = str(len(msg_to_send)).encode(HEADER_FORMAT)
+    # Fill in rest of space
+    msg_length += b' ' * (HEADER_LENGTH - len(msg_length)) 
+    # Send the message length then send the actual message
+    self.client.send(msg_length)
+    self.client.send(msg_to_send)
 
 def main():
-  pass
+  try:
+    client = Client()
+    client.start()
+  except ConnectionRefusedError or ConnectionResetError:
+    print(f"\n[ERROR] server is not running at {SERVER_IP_ADDRESS}:{PORT}")
+  except KeyboardInterrupt:
+    client.send(DISCONNECT_MSG)
+    print("\nGoodbye.")
 
 if __name__ == "__main__":
   main()
